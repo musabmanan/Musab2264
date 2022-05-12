e9386e15b3b21c44d9d2531f7982e5b12bc39fbb
Humaira <>
2022-05-12 18:24:05 -0500

    Initial Commit

---
+++ src/main/webapp/Project2/Project2/src/app/configuration.ts
@@ -0,0 +1,9 @@
+export const configuration = {
+  header: {
+    heading: ' MicroGraphics ',
+    headingtext:
+      'Micrographics offers the highest grade GPUs in the industry, from NVIDIA to AMD, make sure to upgrade your PC today',
+    buttontext: 'View the prices',
+    buttonlink: '\pricing'
+  }
+};
---
+++ src/main/webapp/Project2/Project2/src/polyfills.ts
@@ -0,0 +1,11 @@
+
+import 'core-js/es7/reflect';
+
+
+import 'zone.js';  // Included with Angular CLI.
+
+
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
